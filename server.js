36bf4b485344f6ee039187041397f952b5947774
Prisha Kerin Mercy <162448217+Prishakerinmercy27@users.noreply.github.com>
2026-02-12 13:55:22 +0530

    Remove commented MongoDB connection code
    Removed commented-out MongoDB connection code.

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
